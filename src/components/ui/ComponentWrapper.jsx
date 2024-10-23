8f3c73b5bc21b38ccdd437a1c411ae872b2510ca
tandrimab <bhattacharjee.tan@outlook.com>
2024-10-23 14:11:22 +0530

    Implemented a wrapper component to wrap all components to incorporate reusability

---
+++ src/components/ui/ComponentWrapper.jsx
@@ -0,0 +1,51 @@
+import { Box, ThemeProvider, Typography } from "@mui/material";
+
+ const ComponentWrapper = ({ children }) => {
+  return (
+    <Typography
+      component="div"
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "90vh",
+        overflow: "hidden",
+      }}
+    >
+      <ThemeProvider
+        theme={{
+          palette: {
+            primary: {
+              main: "#9b72cb",
+              dark: "#d96570",
+            },
+          },
+        }}
+      >
+        <Box
+          sx={{
+            width: 400,
+            height: 400,
+            borderRadius: 1,
+            bgcolor: "primary.main",
+            "&:hover": {
+              bgcolor: "primary.dark",
+            },
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            fontSize: "2rem",
+            color: "#ffffff",
+            textAlign: "left",
+            padding: "1.5rem",
+          }}
+          component="div"
+        >
+          {children}
+        </Box>
+      </ThemeProvider>
+    </Typography>
+  );
+};
+
+export default ComponentWrapper
